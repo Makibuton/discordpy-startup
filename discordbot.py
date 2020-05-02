9a8a0e61561d246faef18c014977690534f8daf9
Makibuton <32188883+Makibuton@users.noreply.github.com>
2020-05-02 15:37:11 +0900

    Update discordbot.py
    出力してなかったので修正

--- discordbot.py
+++ discordbot.py
@@ -23,9 +23,10 @@ async def ping(ctx):
 async def weather(ctx):
     resp = urllib.request.urlopen('http://weather.livedoor.com/forecast/webservice/json/v1?city=%s'%citycode).read()
     resp = json.loads(resp.decode('utf-8'))
-    print(resp['location']['city'] + "の天気は")
+    msg = resp['location']['city'] + "の天気は\n"
     for f in resp['forecasts']:
-        print(f['dateLabel'] + "が" + f['telop'])
-    print(resp['description'])
+        msg += f['dateLabel'] + "が" + f['telop'] + "\n"
+    msg += resp['description']
+    await ctx.send(msg)
     
 bot.run(token)
